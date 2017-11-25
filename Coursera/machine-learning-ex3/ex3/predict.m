73cb2fecc4401a1cb5b2f53a8aa7bc99186e89b0
Lukasz Ksiezak <lukasz.ksiezak@interia.pl>
2017-11-25 19:37:03 +0100

    Accuracy improvement by 2-layer neural network

--- Coursera/machine-learning-ex3/ex3/predict.m
+++ Coursera/machine-learning-ex3/ex3/predict.m
@@ -10,26 +10,13 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+X = [ones(m,1) X]; %add bias unit for traininig set
+a_1 = sigmoid(Theta1 * X');
 
+a_1 = a_1';
+a_1 = [ones(m,1) a_1]; %add bias unit for training set for hidden layer
+a_2 = sigmoid(Theta2 * a_1');
 
-
-
-
-
-
-
-
-% =========================================================================
-
-
+[val,p_temp] = max(a_2,[],1);
+p = p_temp';
 end
--- Coursera/machine-learning-ex3/ex3/predictOneVsAll.m
+++ Coursera/machine-learning-ex3/ex3/predictOneVsAll.m
@@ -16,23 +16,7 @@ p = zeros(size(X, 1), 1);
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
-%
-% Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the 
-%       max element, for more information see 'help max'. If your examples 
-%       are in rows, then, you can use max(A, [], 2) to obtain the max 
-%       for each row.
-%       
-% =========================================================================
-
 pred = sigmoid(all_theta * X');
-
 [val,p_temp] = max(pred,[],1);
 p = p_temp';
 end
