430465eb1a98ec0866996683772ada8807e12555
Lukasz Ksiezak <lukasz.ksiezak@interia.pl>
2017-12-17 21:09:11 +0100

    First half of the exercise ex6 working correclty - SVM

--- Coursera/machine-learning-ex6/ex6/dataset3Params.m
+++ Coursera/machine-learning-ex6/ex6/dataset3Params.m
@@ -11,24 +11,32 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+values = [0.01, 0.03, 0.1, 0.3,  1, 3, 10, 30];
 
+results = [];
 
+l = length(values);
 
+for c=1:l,
+    for sigma=1:l,
+      
+      
+      testC = values(c);
+      testSigma = values(sigma);
+      
+      model= svmTrain(X, y, testC, @(x1, x2) gaussianKernel(x1, x2, testSigma)); 
+      predictions = svmPredict(model, Xval);
+      
+      testError = mean(double(predictions ~= yval));
+      
+      results = [results; testC, testSigma, testError];
+      
+    end
+end
 
+[minError, minIndex] = min(results(:,3));
 
-
-
-% =========================================================================
+C = results(minIndex,1);
+sigma = results(minIndex,2);
 
 end
