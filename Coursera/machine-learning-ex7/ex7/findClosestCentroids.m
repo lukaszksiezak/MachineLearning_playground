1359830bbbd9df1a028430ec0d1fdc7988a431ea
Lukasz Ksiezak <lukasz.ksiezak@interia.pl>
2017-12-23 23:45:41 +0100

    Ex7 done - PCA and K-mean

--- Coursera/machine-learning-ex7/ex7/findClosestCentroids.m
+++ Coursera/machine-learning-ex7/ex7/findClosestCentroids.m
@@ -20,11 +20,14 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
+for i=1:size(X,1)
+  dist = zeros(size(centroids,1), 1);
+  for j=1:size(centroids,1)
+    dist(j) = sum(sum((X(i, :) - centroids(j, :)).^ 2));
+  end
+  [val, index] = min(dist);
+  idx(i) = index;
+end
 
 
 % =============================================================
