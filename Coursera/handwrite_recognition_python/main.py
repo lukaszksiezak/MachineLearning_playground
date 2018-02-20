8ec9230093b7458d248988e355a7f3cc7f0d83ba
Lukasz Ksiezak <lukasz.ksiezak@interia.pl>
2018-02-20 20:39:37 +0100

    Separating python code from matlab

--- Coursera/handwrite_recognition_python/main.py
+++
@@ -1,9 +0,0 @@
-import scipy.io
-
-if __name__ == "__main__":
-    
-    input_layer_size  = 400;  # 20x20 Input Images of Digits
-    hidden_layer_size = 25;   # 25 hidden units
-    num_labels = 10;          # 10 labels, from 1 to 10   
-    mat = scipy.io.loadmat('handwrite_recognition_python\ex4data1.mat')
-    
